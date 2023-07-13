edc1a9e0eed6cb873310c75a2519cb332bc1d883
Pranav <pranav.joshi@iitgn.ac.in>
2023-07-13 18:03:56 +0530

    Fixed some things

--- script.js
+++ script.js
@@ -2,16 +2,24 @@ const I = document.getElementById('toggle')
 const SB = document.getElementById('sidebar')
 const C = document.getElementById('C')
 const c = C.getContext('2d')
-var cX,cY
+const H = document.getElementById('heading')
+const h = H.getContext('2d')
+const P = document.getElementById('Page')
+var cX,cY,hX,hY
+var currentSection=0
 S = document.getElementById('sections')
+
 I.onclick = function(){
     if(SB.style.visibility=='visible'){
         SB.style.visibility='hidden'
-        vacateTitleBar()
+        P.style.pointerEvents='all'
+        P.style.opacity='100%'
     }
     else {
         SB.style.visibility='visible'
-        populateTitleBar()
+        P.style.pointerEvents='none'
+        P.style.opacity='50%'
+        rain(100,10,cX/10)
     }
 }
 
@@ -23,20 +31,29 @@ var sections = {
         'CONTACT US':function(){},
     },
     EVENTS:{
-        event1:function(){},
-        event2:function(){},
+        "YOU CAN'T WIN":function(){},
+        "GIVE UP AND DIE":function(){},
     },
     CNS:{
-
+        ";_;":function(){},
     },
     'TECH EXPO':{
-
+        "LOTS of TECH":function(){},
+        "PLAY WITH THE GODDAM THING PLZ":function(){},
     },
     SPONSORS:{
-
+        ";_;":function(){},
     },
 
 }
+var links = {
+    HOME:'https://pj29072004.github.io/SpaceDust/',
+    EVENTS:'https://pj29072004.github.io/Snek/',
+    CNS:'https://pj29072004.github.io/Tunnel/',
+    'TECH EXPO':'link4',
+    SPONSORS:'link5',
+}
+
 for(var section in sections){
     var b;
     for(var l in sections[section]){
@@ -55,10 +72,6 @@ for(var section in sections){
         sections[section][l] = b
     }
 }
-
-
-
-
 var aList = []
 var bList = []
 
@@ -68,8 +81,6 @@ function vacateSideBar(){
     }
     delete aList
     aList = []
-    c.fillStyle = 'black'
-    c.fillRect(0,0,cX,cY)
 }
 
 function vacateTitleBar(){
@@ -80,26 +91,36 @@ function vacateTitleBar(){
     bList = []
 }
 
+function populateSideBar(section){
+    var L = sections[section]
+    var i = 0;
+    for(var l in L){
+        SB.appendChild(L[l])
+        aList.push(L[l])
+        L[l].style.top = `${5*i + 0.5}%`
+        L[l].i = i
+        i += 1
+    }
+    aList[0].click()
+}
+
 function populateTitleBar(){
     var b;
+    var i=0;
     for(var section in sections){
         b = document.createElement('button')
         b.innerText = section
         b.onclick = function(e){
             vacateSideBar()
-            var L = sections[e.target.innerText]
-            var i = 0;
-            for(var l in L){
-                SB.appendChild(L[l])
-                aList.push(L[l])
-                L[l].style.top = `${5*i + 0.5}%`
-                L[l].i = i
-                i += 1
-            }
+            populateSideBar(e.target.innerText)
+            P.src = links[e.target.innerText]
+            currentSection = e.target.i
         }
         b.className = 'tbb'
+        b.i = i
         bList.push(b)
         S.appendChild(b)
+        i++
     }
 }
 
@@ -136,10 +157,49 @@ function hole(y,active=false){
     c.fill()
 }
 
+function drops(n,r){
+    var x,y;
+    for(var i=0;i<n;i++){
+        x = Math.random() * cX
+        y = Math.random() * cY
+        c.beginPath()
+        c.arc(x,y,r,0,2*Math.PI)
+        c.fill()
+    }
+}
+
+function Heading(){
+    var s = Math.floor(Math.min(hY*0.8,hX/10))
+    h.font = `${s}px times-new-roman`
+    h.fillStyle = 'white'
+    h.fillText('AMALTHEA',hX/10,(hY/2) + s/2)
+}
+
+function rain(n,dT,R){
+    c.clearRect(0,0,cX,cY)
+    var dr = R/n
+    var r = dr
+    var inter = setInterval(function(){
+        drops(100,r);
+        r += dr
+        if(r>R){
+            clearInterval(inter)
+            c.fillRect(0,0,cX,cY)
+            bList[currentSection].click()
+        }
+    },dT)
+}
+
 function resize(){
     C.width  = cX = window.innerWidth*0.3
     C.height = cY = window.innerHeight*0.7
+    H.width  = hX = window.innerWidth*0.5
+    H.height = hY = window.innerHeight*0.1
 }
 window.onresize = resize
 resize()
-vacateSideBar()
+populateTitleBar()
+window.onload = function(){
+    bList[0].click()
+    Heading()
+}
